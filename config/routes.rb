59b53a12ee195e333254787d9e20b6e2554a38c6
thanhtruc5000 <trucvlbr50@gmail.com>
2024-12-18 15:10:55 +0700

    add du an truc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "/products", to: "products#index"
 end
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :products
+end
+Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resources :products
+  root "products#index"
 end
